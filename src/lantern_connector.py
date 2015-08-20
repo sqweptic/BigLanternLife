cd592277be9d2f3bf4df100494a5c864cd99337c
Alexander Kurkin <sqweptic@gmail.com>
2015-08-21 00:00:05 +0500

    LanternConnection method to receive lantern data

--- src/lantern_connector.py
+++ src/lantern_connector.py
@@ -1,14 +1,32 @@
+import struct
+
+import tornado.ioloop
+
 from lantern import lantern_instance
 
 class LanternConnector(object):
     def __init__(self, stream_future):
         if stream_future.done() and stream_future.exception() is None:
-            self.set_connection(stream_future.result())
+            self.stream = stream_future.result()
             self.process()
             
-    def set_connection(self, stream):
-        self.stream = stream
-    
     def process(self):
-        print('process connection')
+        self.stream.read_until_close(callback = self.receive_raw_data)
+    
+    def close(self):
+        self.stream.close()
+        tornado.ioloop.IOLoop.current().stop()
         
+    def receive_raw_data(self, data):
+        print('data get')
+        value_len = len(data) - 3
+        command, length, raw_value = struct.unpack('>BH' + str(value_len) + 's', 
+                                                   data)
+        
+        value = raw_value[:length]
+        
+        print(command, length, len(value))
+        
+        self.close()
+        
+
